94d15d591f92599005798bf6e15d1d0ac9b00c64
Bran Liang <bran@corran.cn>
2020-09-02 19:48:11 +0800

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by BranLiang on 2020/9/2.
+//  Copyright © 2020 Corran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
